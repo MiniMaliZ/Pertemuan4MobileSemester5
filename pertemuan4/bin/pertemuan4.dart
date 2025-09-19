8048d68980cb7a145addab9b52bf6cf07c0545be
MiniMaliZ <gakisomlebu@gmail.com>
2025-09-19 13:33:45 +0700

    List Input

--- pertemuan4/bin/pertemuan4.dart
+++ pertemuan4/bin/pertemuan4.dart
@@ -3,16 +3,35 @@ import 'dart:io';
 import 'package:pertemuan4/pertemuan4.dart' as pertemuan4;
 
 void main(List<String> arguments) {
-  var fl = List<int>.filled(4, 0);
+  // var fl = List<int>.filled(4, 0);
 
-  fl[0] = 1;
-  fl[1] = 2;
-  fl[2] = 3;
-  fl[3] = 4;
+  // fl[0] = 1;
+  // fl[1] = 2;
+  // fl[2] = 3;
+  // fl[3] = 4;
 
   // stdout.writeln(fl);
 
   // var gl = [1,2,3];
   // gl.add(4);
-  // gl.re
+  // gl.remove(2);
+
+  // stdout.writeln(gl);
+
+  var setNilai1 = <int>{};
+  var setNilai2 = <int>{3,3,4,5};
+
+  stdout.write("Jumlah data setNilai1 : ");
+  String? input = stdin.readLineSync();
+  int jumlah = int.tryParse(input ?? '0') ?? 0;
+  for (var i = 0; i < jumlah; i++) {
+    stdout.write("Masukkan nilai ke-${i+1}: ");
+    String? nilaiInput = stdin.readLineSync();
+    int nilai = int.tryParse(nilaiInput ?? '0') ?? 0;
+    setNilai1.add(nilai);
+  }
+
+  print(setNilai1);
+  print(setNilai1.union(setNilai2));
+  print(setNilai1.intersection(setNilai2));
 }
